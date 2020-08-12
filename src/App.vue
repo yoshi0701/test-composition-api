f58c259fc1f13b225f4f692a4e8c322a2ef0474b
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-08-12 22:34:40 +0900

    feat: components implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+    <Header :title="'Composition API'" />
+    <Search :search="state.search" @search="handleSearch" />
+    <p class="App-intro">Sharing a few of our favourite movies</p>
+    <div class="movies">
+      <Movie v-for="movie in state.movies" :movie="movie" :key="movie.imdbID" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import { reactive, watch } from '@vue/composition-api';
+  import Header from './components/TheHeader.vue';
+  import Search from './components/Search.vue';
+  import Movie from './components/Movie.vue';
+
+  const API_KEY = 'a5549d08';
+
+  export default {
+    name: 'App',
+    components: {
+      Header, Search, Movie
+    },
+    setup() {
+      const state = reactive({
+        search: 'Joker',
+        loading: true,
+        movies: [],
+        errorMessage: null
+      });
+
+      watch(() => {
+        const MOVIE_API_URL = `https://www.omdbapi.com/?s=${state.search}&apikey=${API_KEY}`;
+
+        fetch(MOVIE_API_URL)
+          .then(response => response.json())
+          .then(jsonResponse => {
+            state.movies = jsonResponse.Search;
+            state.loading = false;
+          });
+      });
+
+      return {
+        state,
+        handleSearch(searchTerm) {
+          state.loading = true;
+          state.search = searchTerm;
+        }
+      };
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
